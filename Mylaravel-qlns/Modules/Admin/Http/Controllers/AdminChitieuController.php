bd43052ee05444490e24bba3c1d472e9aef8ea4e
Nguyễn Văn Thành <48385193+congnghecao@users.noreply.github.com>
2019-08-09 10:20:14 +0700

    Update - Index Chitieu
    - Thông báo Add, Update
    - check box Phong ban

--- Mylaravel-qlns/Modules/Admin/Http/Controllers/AdminChitieuController.php
+++ Mylaravel-qlns/Modules/Admin/Http/Controllers/AdminChitieuController.php
@@ -21,19 +21,13 @@ class AdminChitieuController extends Controller
         $chitieu = Chitieu::paginate(7);
         $phong = Phongban::all();
 
-
         $viewData = [
             'chitieu' => $chitieu,
-                'phong' => $phong
+            'phong' => $phong
         ];
 
         return view('admin::chitieu.index', $viewData);
     }
-
-//    public function getCreate(){
-//        $phongban = Phongban::all();
-//        return view('admin::chitieu.create',compact('phongban'));
-//    }
 
     public function PostCreate(RequestChitieu $requestChitieu)
     {
@@ -42,15 +36,14 @@ class AdminChitieuController extends Controller
 
         $chitieu->ten_chi_tieu = $requestChitieu->tenchitieu;
         $chitieu->mo_ta = $requestChitieu->mota;
-        $chitieu->id_phong_ban = implode(",",$requestChitieu->check);
+        if ($requestChitieu->check != "") {
+            $chitieu->id_phong_ban = implode(",", $requestChitieu->check);
+        } else {
+            $chitieu->id_phong_ban = "Chỉ tiêu không được chọn cho phòng ban nào?";
+        }
         $chitieu->save();
-        return redirect()->back();
+        return redirect()->back()->with('statusadd', 'Thêm mới thành công!');
     }
-
-//    public function getUpdate($id){
-//        $chitieu = Chitieu::find($id);
-//        return view('admin::chitieu.update',compact('chitieu'));
-//    }
 
     public function postUpdate(Request $request)
     {
@@ -59,15 +52,14 @@ class AdminChitieuController extends Controller
 
         $chitieu->ten_chi_tieu = $request->ten;
         $chitieu->mo_ta = $request->mo;
+        if ($request->checkupdate != "") {
+            $chitieu->id_phong_ban = implode(",", $request->checkupdate);
+        } else {
+            $chitieu->id_phong_ban = "Chỉ tiêu không được chọn cho phòng ban nào?";
+        }
         $chitieu->save();
-        return redirect()->back();
+        return redirect()->back()->with('statusupdate', 'Cập nhật thành công!');
     }
-//
-//    public function delete($id){
-//        $chitieu = Chitieu::find($id);
-//        $chitieu->delete();
-//        return redirect()->back();
-//    }
 
     public function danhsachchitieu()
     {
@@ -93,70 +85,73 @@ class AdminChitieuController extends Controller
 
 
     //Nhaan su
-    public function getIndexNhansu(){
+    public function getIndexNhansu()
+    {
         $date = getdate();
-        $thang= $date['mon'];
+        $thang = $date['mon'];
         $nam = $date['year'];
 
 
-
         $thangnam = DB::table('chitietchitieu')
-            ->select('thang','nam')
-            ->groupBy('thang','nam')
-            ->get();
-
-      $nhansuchitieu = DB::table('chitietchitieu')
-            ->join('nhansu','nhansu.id','=','chitietchitieu.id_nhan_su')
-            ->select('ho_ten','id_nhan_su','thang',DB::raw('SUM(diem_chi_tieu) as sumdct'),DB::raw('SUM(diem_dat_duoc) as sumddd'))
-            ->where('thang',$thang)
-          ->where('nam',$nam)
-            ->groupBy('id_nhan_su','thang','ho_ten')
-            ->get();
-       $nhansu = DB::table('nhansu')
-           ->join('lamviec','nhansu.id','=','lamviec.id_nhan_su')
-           ->select('nhansu.id as idns','ho_ten')
-           ->where('lamviec.ngay_ket_thuc',null)
-           ->get();
-        return view('admin::chitieu.chitieuNhansuIndex',compact('nhansu','thang','nam','nhansuchitieu','thangnam'));
-    }
-
-    public function postIndexNhansu(Request $request){
-            $thang = $request->sheachthang;
-            $nam = $request->sheachnam;
-        $thangnam = DB::table('chitietchitieu')
-            ->select('thang','nam')
-            ->groupBy('thang','nam')
+            ->select('thang', 'nam')
+            ->groupBy('thang', 'nam')
             ->get();
 
         $nhansuchitieu = DB::table('chitietchitieu')
-            ->join('nhansu','nhansu.id','=','chitietchitieu.id_nhan_su')
-            ->select('ho_ten','id_nhan_su','thang',DB::raw('SUM(diem_chi_tieu) as sumdct'),DB::raw('SUM(diem_dat_duoc) as sumddd'))
-            ->where('thang',$thang)
-            ->where('nam',$nam)
-            ->groupBy('id_nhan_su','thang','ho_ten')
+            ->join('nhansu', 'nhansu.id', '=', 'chitietchitieu.id_nhan_su')
+            ->select('ho_ten', 'id_nhan_su', 'thang', DB::raw('SUM(diem_chi_tieu) as sumdct'), DB::raw('SUM(diem_dat_duoc) as sumddd'))
+            ->where('thang', $thang)
+            ->where('nam', $nam)
+            ->groupBy('id_nhan_su', 'thang', 'ho_ten')
             ->get();
         $nhansu = DB::table('nhansu')
-            ->join('lamviec','nhansu.id','=','lamviec.id_nhan_su')
-            ->select('nhansu.id as idns','ho_ten')
-            ->where('lamviec.ngay_ket_thuc',null)
+            ->join('lamviec', 'nhansu.id', '=', 'lamviec.id_nhan_su')
+            ->select('nhansu.id as idns', 'ho_ten')
+            ->where('lamviec.ngay_ket_thuc', null)
             ->get();
-        return view('admin::chitieu.chitieuNhansuIndex',compact('nhansu','thang','nam','nhansuchitieu','thangnam'));
+        return view('admin::chitieu.chitieuNhansuIndex', compact('nhansu', 'thang', 'nam', 'nhansuchitieu', 'thangnam'));
+    }
+
+    public function postIndexNhansu(Request $request)
+    {
+        $thang = $request->sheachthang;
+        $nam = $request->sheachnam;
+        $thangnam = DB::table('chitietchitieu')
+            ->select('thang', 'nam')
+            ->groupBy('thang', 'nam')
+            ->get();
+
+        $nhansuchitieu = DB::table('chitietchitieu')
+            ->join('nhansu', 'nhansu.id', '=', 'chitietchitieu.id_nhan_su')
+            ->select('ho_ten', 'id_nhan_su', 'thang', DB::raw('SUM(diem_chi_tieu) as sumdct'), DB::raw('SUM(diem_dat_duoc) as sumddd'))
+            ->where('thang', $thang)
+            ->where('nam', $nam)
+            ->groupBy('id_nhan_su', 'thang', 'ho_ten')
+            ->get();
+        $nhansu = DB::table('nhansu')
+            ->join('lamviec', 'nhansu.id', '=', 'lamviec.id_nhan_su')
+            ->select('nhansu.id as idns', 'ho_ten')
+            ->where('lamviec.ngay_ket_thuc', null)
+            ->get();
+        return view('admin::chitieu.chitieuNhansuIndex', compact('nhansu', 'thang', 'nam', 'nhansuchitieu', 'thangnam'));
 
     }
 
-    public function getIndexNhansuChitieu($id,$thang,$nam){
+    public function getIndexNhansuChitieu($id, $thang, $nam)
+    {
         $chitieu = Chitieu::all();
 
         $chitietchitieu = DB::table('nhansu')
-        ->leftJoin('chitietchitieu','chitietchitieu.id_nhan_su','=','nhansu.id')
-        ->leftJoin('chitieu','chitieu.id','=','chitietchitieu.id_chi_tieu')
-        ->select('chitietchitieu.id','nhansu.id as idnhansu','chitieu.id as idchitieu','thang','ten_chi_tieu','diem_chi_tieu','diem_dat_duoc')
-         ->where('nhansu.id',$id)
-        ->get();
-      return view('admin::chitieu.chitieuNhansu',compact('chitietchitieu','thang','nam','chitieu'));
+            ->leftJoin('chitietchitieu', 'chitietchitieu.id_nhan_su', '=', 'nhansu.id')
+            ->leftJoin('chitieu', 'chitieu.id', '=', 'chitietchitieu.id_chi_tieu')
+            ->select('chitietchitieu.id', 'nhansu.id as idnhansu', 'chitieu.id as idchitieu', 'thang', 'ten_chi_tieu', 'diem_chi_tieu', 'diem_dat_duoc')
+            ->where('nhansu.id', $id)
+            ->get();
+        return view('admin::chitieu.chitieuNhansu', compact('chitietchitieu', 'thang', 'nam', 'chitieu'));
     }
 
-    public function postCreateNhansuChitieu(RequestChitieuNhansu $requestChitieuNhansu){
+    public function postCreateNhansuChitieu(RequestChitieuNhansu $requestChitieuNhansu)
+    {
         $chitietchitieu = new Chitietchitieu();
         $chitietchitieu->id_chi_tieu = $requestChitieuNhansu->idchitieu;
         $chitietchitieu->id_nhan_su = $requestChitieuNhansu->idnhansu;
@@ -167,7 +162,8 @@ class AdminChitieuController extends Controller
         return redirect()->back();
     }
 
-    public function postUpdateNhansuChitieu(Request $request){
+    public function postUpdateNhansuChitieu(Request $request)
+    {
         $chitietchitieu = Chitietchitieu::find($request->chitieuNhansu_id);
         $chitietchitieu->id_chi_tieu = $request->idchitieu;
         $chitietchitieu->id_nhan_su = $request->idnhansu;
@@ -182,7 +178,8 @@ class AdminChitieuController extends Controller
         return redirect()->back();
     }
 
-    public function getDeleteNhansuChitieu($id){
+    public function getDeleteNhansuChitieu($id)
+    {
         $chitietchitieu = Chitietchitieu::find($id);
         $chitietchitieu->delete();
         return redirect()->back();
--- Mylaravel-qlns/Modules/Admin/Resources/views/chitieu/index.blade.php
+++ Mylaravel-qlns/Modules/Admin/Resources/views/chitieu/index.blade.php
@@ -9,42 +9,45 @@
                     <h1><i class="app-menu__icon fa fa-laptop"></i> Danh sách Chỉ tiêu</h1>
                 </div>
                 <div class="col-md-3">
-                    {{--<a href="{{route('admin.get.create.chitieu')}}" class="btn btn-primary" style="border-radius: 10px;" type="button">Thêm Mới</a>--}}
                     <a href="" class="btn btn-primary" style="border-radius: 10px;" type="button" data-toggle="modal"
                        data-target="#add">Thêm Mới</a>
                     <div class="modal fade" id="add" role="dialog">
-                        <div class="modal-dialog">
+                        <div class="modal-dialog modal-lg">
                             <!-- Modal content-->
                             <div class="modal-content">
                                 <div class="pd" style="margin: 20px;">
                                     <h3 style="text-shadow: 1px 1px 1px #2a383e;margin-top: 45px;margin-bottom: 45px;font-size: 30px;font-weight: 100;"><span style="color: red;font-size: 40px;">|</span>Điền thông tin Chỉ tiêu:</h3>
                                     <form action="{{route('admin.get.create.chitieu')}}" method="post">
                                         @csrf
-                                        <p><b>Tên chỉ tiêu:</b></p>
-                                        <input class="form-control is-valid" id="inputValid" type="text" value="{{old('tenchitieu')}}" name="tenchitieu" style="width: 60%">
-                                        @if($errors->has('tenchitieu'))
-                                            <span style="font-size: 12px;color: red"><i>{{$errors->first('tenchitieu')}}</i></span>
-                                        @endif
-                                        <br>
-                                        <p><b>Mô tả:</b></p>
-                                        <textarea class="form-control" id="exampleTextarea" rows="3" type="text"  name="mota">{{old('mota')}}</textarea>
-                                        {{--<input class="form-control is-valid" id="inputValid" type="text" value="{{old('mota')}}" name="mota">--}}
-
-                                        @if($errors->has('mota'))
-                                            <span style="font-size: 12px;color: red"><i>{{$errors->first('mota')}}</i></span>
-                                        @endif
-                                        <br>
-                                        <p><b>Phòng ban:</b></p>
-                                        <input style="margin-left: 40px; margin-top: 10px;" type="checkbox" id="select_all"/> Chọn tất cả<br>
                                         <div class="row">
-                                            @foreach($phong as $p)
-                                                <div class="col-md-6">
-                                                    <input class="checkbox" style="margin-left: 40px; margin-top: 10px;" type="checkbox" name="check[]" value="{{$p->id}}"> {{$p->ten_phong}}
+                                            <div class="col-md-6">
+                                                <p><b>Tên chỉ tiêu:</b></p>
+                                                <input class="form-control is-valid" id="inputValid" type="text" value="{{old('tenchitieu')}}" name="tenchitieu" style="width: 60%">
+                                                @if($errors->has('tenchitieu'))
+                                                    <span style="font-size: 12px;color: red"><i>{{$errors->first('tenchitieu')}}</i></span>
+                                                @endif
+                                                <br>
+                                                <p><b>Mô tả:</b></p>
+                                                <textarea class="form-control" id="exampleTextarea" rows="3" type="text"  name="mota">{{old('mota')}}</textarea>
+                                                @if($errors->has('mota'))
+                                                    <span style="font-size: 12px;color: red"><i>{{$errors->first('mota')}}</i></span>
+                                                @endif
+                                                <br>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p><b>Phòng ban:</b></p>
+                                                <input style=" margin-top: 10px;" type="checkbox" id="select_all"/> Chọn tất cả<br>
+                                                <div class="row">
+                                                    @foreach($phong as $p)
+                                                        <div class="col-md-6">
+                                                            <input class="checkbox" style=" margin-top: 10px;" type="checkbox" name="check[]" value="{{$p->ten_phong}}"> {{$p->ten_phong}}
+                                                        </div>
+                                                    @endforeach
                                                 </div>
-
-
-                                            @endforeach
+                                            </div>
                                         </div>
+
+                                        <p style="color: red;">*Lưu ý: Phải click thuộc phòng ban nào?</p>
                                         <center>
                                             <button class="btn btn-primary" type="submit">Thêm</button>
                                         </center>
@@ -58,6 +61,30 @@
 
             </div>
             <ul class="app-breadcrumb breadcrumb">
+                <div style="position: absolute;top: 70px;right: 180px;">
+                    @if (session('statusadd'))
+                        <div class="chuthich right wow slideInRight" data-wow-duration="1s" style="width: 300px;display: block;">
+                            <div class="arroww" style="top: 50%;"></div>
+                            <h3 class="chuthich-title" style="color:red;">Thông báo!</h3>
+                            <div class="chuthich-content">{{ session('statusadd') }}</div>
+                        </div>
+                    @elseif (session('statusupdate'))
+                        <div class="chuthich right wow slideInRight" style="width: 300px;display: block;" data-wow-duration="1s">
+                            <div class="arroww" style="top: 50%;"></div>
+                            <h3 class="chuthich-title" style="color:red;">Thông báo!</h3>
+
+                            <div class="chuthich-content">{{ session('statusupdate') }}</div>
+                        </div>
+                    @else
+                        <div class="chuthich right " style="width: 300px;display: block;">
+                            <div class="arroww" style="top: 50%;"></div>
+                            <h3 class="chuthich-title" style="color:red;">Thông báo!</h3>
+
+                            <div class="chuthich-content">Không có thông báo!</div>
+                        </div>
+                    @endif
+                </div>
+                <li class="breadcrumb-item"><a><i class="fa fa-bell-o fa-lg"></i></a></li>
                 <li class="breadcrumb-item"><a href="{{route('admin.trangchu')}}"> <i class="fa fa-home fa-lg"></i></a></li>
                 <li class="breadcrumb-item"><a href="#">Chỉ tiêu</a></li>
             </ul>
@@ -67,15 +94,6 @@
                 <div class="tile">
                     <div class="tile-body">
                         <div id="sampleTable_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4 no-footer">
-                            <div class="row">
-                                {{--<div class="col-sm-12 col-md-6">--}}
-                                {{--</div>--}}
-                                {{--<div class="col-sm-12 col-md-6">--}}
-                                    {{--<div id="sampleTable_filter" class="dataTables_filter"><label>Tìm kiếm:<input--}}
-                                                    {{--type="search" class="form-control form-control-sm" placeholder=""--}}
-                                                    {{--aria-controls="sampleTable"></label></div>--}}
-                                {{--</div>--}}
-                            </div>
                             <div class="row">
                                 <div class="col-sm-12">
                                     <table class="table table-hover table-bordered dataTable no-footer" id="sampleTable"
@@ -87,19 +105,16 @@
                                                 aria-label="" style="width: 5%;">
                                                 Mã
                                             </th>
-                                            {{--<th class="sorting_asc" tabindex="" aria-controls="" rowspan=""--}}
-                                                {{--colspan="" aria-sort=""--}}
-                                                {{--aria-label="" style="width: 12%;">--}}
-                                                {{--Mã phòng ban--}}
-                                            {{--</th>--}}
+
                                             <th class="sorting" tabindex="" aria-controls="" rowspan=""
                                                 colspan="" aria-label=""
                                                 style="width: 30%;">Tên chỉ tiêu
                                             </th>
-                                            {{--<th class="sorting" tabindex="" aria-controls="" rowspan=""--}}
-                                                {{--colspan="" aria-label=""--}}
-                                                {{--style="width: 30%;">Mã phòng ban--}}
-                                            {{--</th>--}}
+                                            <th class="sorting_asc" tabindex="" aria-controls="" rowspan=""
+                                                colspan="" aria-sort=""
+                                                aria-label="" style="width: 30%;">
+                                                Thuộc phòng ban
+                                            </th>
                                             <th class="sorting" tabindex="" aria-controls="" rowspan=""
                                                 colspan="" aria-label=""
                                                 style="width: 40%;">Mô tả
@@ -115,12 +130,11 @@
                                         @foreach($chitieu as $ct)
                                             <tr role="row" class="odd">
                                                 <td class="sorting_1">{{$ct->id}}</td>
-                                                {{--<td>{{$ct->id_phong_ban}}</td>--}}
                                                 <td>{{$ct->ten_chi_tieu}}</td>
-                                                {{--<td>{{$ct->id_phong_ban}}</td>--}}
+                                                <td>{{$ct->id_phong_ban}}</td>
                                                 <td>{{$ct->mo_ta}}</td>
                                                 <td>
-                                                    <button  data-catid="{{$ct->id}}" data-mytitle="{{$ct->ten_chi_tieu}}" data-mydescriptiton="{{$ct->mo_ta}}" class="btn btn-primary" style="color: white;font-size: 10px;border: 20px;" type="button" data-toggle="modal" data-target="#update">Cập nhật</button>
+                                                    <button data-catid="{{$ct->id}}" data-mytitle="{{$ct->ten_chi_tieu}}" data-mydescriptiton="{{$ct->mo_ta}}" class="btn btn-primary" style="color: white;font-size: 10px;border: 20px;" type="button" data-toggle="modal" data-target="#update">Cập nhật</button>
                                                 </td>
                                             </tr>
                                         @endforeach
@@ -141,7 +155,7 @@
                                 </div>
                             </div>
                             <div class="modal fade" id="update" role="dialog" aria-labelledby="myModalLabel">
-                                <div class="modal-dialog" role="document">
+                                <div class="modal-dialog modal-lg" role="document">
                                     <!-- Modal content-->
                                     <div class="modal-content">
                                         <div class="modal-body pd" style="margin: 20px;">
@@ -151,13 +165,28 @@
                                                 @csrf
                                                 <input type="hidden" name="chitieu_id" id="cat_id" value="">
 
-                                                <p><b>Tên chỉ tiêu:</b></p>
-                                                <input class="form-control is-valid" id="title" type="text" value="" name="ten">
-                                                <br>
-                                                <p><b>Mô tả:</b></p>
-                                                <textarea class="form-control" id="des" rows="3" type="text"  name="mo"></textarea>
-                                                {{--<input class="form-control is-valid" id="des" type="text" value="" name="mo">--}}
-                                                <br>
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <p><b>Tên chỉ tiêu:</b></p>
+                                                        <input class="form-control is-valid" id="title" type="text" value="" name="ten">
+                                                        <br>
+                                                        <p><b>Mô tả:</b></p>
+                                                        <textarea class="form-control" id="des" rows="3" type="text"  name="mo"></textarea>
+                                                        <br>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <p><b>Phòng ban:</b></p>
+                                                        <input style=" margin-top: 10px;" type="checkbox" id="select_all_update"/> Chọn tất cả<br>
+                                                        <div class="row">
+                                                            @foreach($phong as $p)
+                                                                <div class="col-md-6">
+                                                                    <input class="checkboxupdate" style=" margin-top: 10px;" type="checkbox" name="checkupdate[]" value="{{$p->ten_phong}}"> {{$p->ten_phong}}
+                                                                </div>
+                                                            @endforeach
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <p style="color: red;">*Lưu ý: Phải click thuộc phòng ban nào?</p>
                                                 <center>
                                                     <button class="btn btn-primary" type="submit">Cập nhật</button>
                                                 </center>
